3648817b9f937e31fdd4d54d3243ffca17329396
CIRO QUINTERO <ciroalfonsoqude@ufps.edu.co>
2022-10-04 15:58:39 -0500

    Elimino archivos viejos y agrego tabla de de productos

--- scrip.js
+++ scrip.js
@@ -1,10 +1,47 @@
-$("#login").click(function(event){
+$("#registrase").click(function(event){
     $("#contenedor").load('loginyregister.html')
 });
 
-$("#hombre").click(function(event){
-    $("#contenedor").load('cuenta/categoria.html')
+$("#cuentaAdmin").click(function(event){
+    $("#contenedor").load('cuenta/admin.html')
 });
+var boton = document.getElementById('agregar_p');
+var guardar = document.getElementById('guardar');
+var lista = document.getElementById('lista');
+var data = [];
+var cant = 0;
+boton.addEventListener("click", agregar_producto);
+guardar.addEventListener("click", save);
+function agregar_producto() {
+    var nombre = document.getElementById('nombre').value;
+    var precio = parseFloat(document.getElementById('precio').value);
+    var cantidad = parseFloat(document.getElementById('cantidad').value);
+
+    var total = precio * cantidad;
+    data.push(
+        {
+            "id": cant,
+            "nombre": precio,
+            "cantidad": cantidad,
+            "total": total
+        }
+    );
+    var id_row = 'row' + cant;
+    var fila = '<tr id=' + id_row + '><td>' + nombre + '</td><td>' + precio + '</td><td>' + cantidad + '</td><td>' + total + '</td><td><a hrf="#" class="btn btn-danger" onclick"eliminar('
+        + cant + ')";>Eliminar</a><a href"#" class="btn btn-warning" onclick="cantidad(' + cant + ')";>Cantidad</a></td></tr>';
+
+    $("#lista").append(fila);
+    $("#nombre").val('');
+    $("#precio").val('');
+    $("#cantidad").val('');
+    $("#nombre").focus();
+    cant++;
+
+}
+function save() {
+
+}
+
 
   
 
