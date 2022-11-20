ced8eb18fa332dad4a6f5346f2c2ae77878cba38
cquintero99 <ciroalfonsoqude@ufps.edu.co>
2022-11-19 19:04:50 -0500

    modifico metodo post usuarios

--- usuario.js
+++ usuario.js
@@ -2,7 +2,7 @@
 
 
 $("#usuarios").click(function(event){
-    fetch('http://localhost:8080/usuarios/lista')
+    fetch('http://localhost:8090/usuarios/lista')
     .then(response => response.json())
     .then(data => mostrarUsuarios(data))
     .catch(error => console.log(error))
@@ -11,7 +11,7 @@ $("#usuarios").click(function(event){
         console.log(data)
         let body=''
         for(let i=0;i<data.length;i++){
-         body+=`<tr>  <td>${data[i].cedula}</td><td>${data[i].nombre}</td><td>${data[i].apellido}</td> <td>${data[i].correo}</td> <td>${data[i].sexo}</td> <td>${data[i].estado}</td></tr>`
+         body+=`<tr>  <td>${data[i].cedula}</td><td>${data[i].nombre}</td><td>${data[i].apellido}</td> <td>${data[i].correo}</td> <td>${data[i].sexo}</td> <td>${data[i].idestado}</td></tr>`
          
         }
         document.getElementById('dataUsuarios').innerHTML=body
